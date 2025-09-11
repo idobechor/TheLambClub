1a3fce665e629a6244407da3e32e42f3bbf88d52
Ido <Ido@DESKTOP-7LIPMI1>
2025-09-11 21:29:36 +0300

    Add project files.

---
+++ TheLambClub/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TheLambClub
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TheLambClub/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TheLambClub
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
