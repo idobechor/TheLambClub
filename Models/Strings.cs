b0dfb3d856e3ba042181be47abb2db3368bcc938
Idobechor <Ido.Bechor2008@gmail.com>
2025-09-28 20:24:20 +0300

    ui changes

--- Models/Strings.cs
+++ Models/Strings.cs
@@ -6,6 +6,10 @@
         public const string Register = "Register";
         public const string LoginText = "Log in";
         public const string UserName = "User name";
+        public const string EnterPassword = "Enter Password:";
+        public const string EnterUserName = "Enter UserName:";
+        public const string EnterEmail = "Enter Email:";
+        public const string EnterAge = "Enter Age:";
         public const string Password = "Password";
         public const string Email = "Email";
         public const string Age = "Age";
